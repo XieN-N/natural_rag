737fd5d4e424cded6fb708aa496b239adbcceb2e
Stanislav Shtuka <52897594+XieN-N@users.noreply.github.com>
2026-03-23 19:11:19 +1000

    Update __init__.py

--- natural_rag/pipelines/__init__.py
+++ natural_rag/pipelines/__init__.py
@@ -5,7 +5,26 @@ from typing import Any
 from natural_rag.data import Document
 from natural_rag.dataset import RAGDataset
 
-
 class RAGPipeline(ABC):
     def build_index(self, documents: list[Document]): ...
     def generate_answer(self, question: str) -> tuple[str, Any]: ...
+
+try:
+    from natural_rag.pipelines.ragu_pipelines import RAGUPipeline
+except ImportError:
+    pass
+
+try:
+    from natural_rag.pipelines.lightrag_pipelines import LightRAGPipeline
+except ImportError:
+    pass
+
+try:
+    from natural_rag.pipelines.nano_graphrag_pipelines import NanoGraphRAGPipeline
+except ImportError:
+    pass
+
+try:
+    from natural_rag.pipelines.fast_graphrag_pipelines import FastGraphRAGPipeline
+except ImportError:
+    pass
